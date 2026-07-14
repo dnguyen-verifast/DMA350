df70c4539a4be2b0cb6610aa3df1e1e1a7e24911
dnguyen-verifast <dnguyen@verifasttech.com>
2026-07-14 15:45:35 +0700

    dma350_single_copy_test

--- testbench/test/vseq/dma350_vseq_1d_single_wrap.sv
+++ testbench/test/vseq/dma350_vseq_1d_single_wrap.sv
@@ -15,8 +15,9 @@ class dma350_vseq_1d_single_wrap extends dma350_vseq_base;
     apb_write(ch_addr(0,O_SRCADDR),  32'h0000_6000);
     apb_write(ch_addr(0,O_SRCADDRHI),32'h0);
     apb_write(ch_addr(0,O_DESADDR),  32'h0000_7000);
+  
     apb_write(ch_addr(0,O_DESADDRHI),32'h0);
-    apb_write(ch_addr(0,O_XSIZE),    {16'd16, 16'd16});
+    apb_write(ch_addr(0,O_XSIZE),    {16'd16, 16'd8});
     apb_write(ch_addr(0,O_XADDRINC), 32'h0001_0001);   // src/des +1 element
     // TRANSIZE=word(010), XTYPE=wrap(010), DONETYPE=end-of-command
     apb_write(ch_addr(0,O_CTRL), (32'h1<<21) | (32'h2<<9) | 32'h2);
