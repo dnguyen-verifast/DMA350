9662ac89ec46a402f4fc1abe2541247a95e360b9
dnguyen-verifast <dnguyen@verifasttech.com>
2026-07-21 15:14:18 +0700

    trigger

--- testbench/dma_env/dma350_scoreboard.sv
+++ testbench/dma_env/dma350_scoreboard.sv
@@ -758,8 +758,8 @@ class dma350_scoreboard extends uvm_scoreboard;
         `uvm_info("SB_PRED_SIDE", $sformatf("total_beats = %d ",rem),UVM_LOW)
         while (rem > 0) begin
             int nb = calc_beats(rem, cur, size, eff_fixed, max_beats);
-            `uvm_info("SB_PRED_SIDE", $sformatf("calc_beats= %d: rem, cur, size, eff_fixed, max_beats = %d"
-                                                max_beats, nb),UVM_LOW)
+
+            `uvm_info("SB_PRED_SIDE", $sformatf("calc_beats= %d: rem, cur, size, eff_fixed, max_beats = %d", max_beats, nb),UVM_LOW)
 
             bd = dma_axi_burst::type_id::create("bd");
             bd.addr = cur; bd.beats = nb; bd.size = size; bd.fixed = eff_fixed;
